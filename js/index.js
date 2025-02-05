86803b46265d440aa83e6e2806bd874972665bef
Olegoriy <olegkib1506@gmail.com>
2025-02-05 16:59:55 +0300

    add: start script for modal window

---
+++ js/index.js
@@ -0,0 +1,32 @@
+"use strict"
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    // Modal
+
+    const buttonsModal = document.querySelectorAll('.order__call'),
+        modal = document.querySelector('.modal');
+
+    function openModal() {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+    }
+
+    function closeModal() {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = '';
+    }
+
+    buttonsModal.forEach((btn) => {
+        btn.addEventListener('click', openModal)
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code == "Escape" && modal.classList.contains('show')) {
+            closeModal();
+        }
+    });
+
+});
