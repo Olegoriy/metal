2a5db4c9592988920ec083cf69a9122ac4053345
Olegoriy <olegkib1506@gmail.com>
2025-02-15 12:00:54 +0300

    add: start script for submit data from modal

--- js/index.js
+++ js/index.js
@@ -128,4 +128,43 @@ window.addEventListener('DOMContentLoaded', () => {
                 slideIndex--;
             }
         });
+
+
+        // Forms 
+
+        const forms = document.querySelectorAll('form');
+
+        forms.forEach(item => {
+            bindPostData(item);
+        });
+        
+        const postData = async (url, data) => {
+            const res = await fetch(url, {
+                method: "POST",
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: data
+            });
+        
+            return await res.json();
+        };
+
+        function bindPostData(form) {
+            form.addEventListener('submit', (e) => {
+                e.preventDefault();
+        
+                const formData = new FormData(form);
+        
+                const json = JSON.stringify(Object.fromEntries(formData.entries()));
+        
+                postData('http://localhost:3000/requests', json)
+                .then(data => {
+                    console.log(data);
+                    form.reset();
+                }).finally(() => {
+                    form.reset();
+                })
+            });
+        }
 });
